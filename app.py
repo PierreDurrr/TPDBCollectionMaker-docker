c46847ee8bacad84e0ad3d8fbc230e1b461de94d
PierreDurrr <73993035+PierreDurrr@users.noreply.github.com>
2023-11-29 00:21:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,74 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
-import os
-import requests
 import subprocess
-import time
-import jinja2
+import requests
 
 app = Flask(__name__)
 
-# Create Jinja2 template loader
-template_loader = jinja2.Environment(loader=jinja2.FileSystemLoader("/app/templates"))
-
 @app.route('/')
 def index():
-    folder_contents = os.listdir('/app/data')
-    return render_template('index.html', folder_contents=folder_contents)
+    return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    uploaded_file = request.files['file']
+@app.route('/parse', methods=['POST'])
+def parse_url():
     url = request.form.get('url')
-    custom_output_name = request.form.get('output_name')
-    
-    if uploaded_file.filename != '':
-        file_path = os.path.join('/app/data', uploaded_file.filename)
-        uploaded_file.save(file_path)
-        return redirect(url_for('index', success=True, filename=uploaded_file.filename))
-    elif url:
-        # Handle URL input
-        try:
-            response = requests.get(url)
-            html_content = response.text
 
-            # Save HTML content to a temporary file
-            temp_file_path = '/app/data/temp_html_file.html'
-            with open(temp_file_path, 'w') as temp_file:
-                temp_file.write(html_content)
-
-            # Execute the command for the temporary HTML file
-            execute_command(temp_file_path, custom_output_name)
-            
-            return redirect(url_for('index', success=True, filename=custom_output_name or 'temp_html_file.html'))
-        except Exception as e:
-            print(f"Error fetching HTML content from URL: {e}")
-            return redirect(url_for('index', success=False))
-    return redirect(url_for('index', success=False))
-
-def execute_command(file_path, custom_output_name=None):
-    # Modify the command to include the file path
-    full_command = ["python3", "/app/TPDBCollectionMaker/main.py", file_path, "--always-quote"]
- 
     try:
-        print(f"Waiting for 10 seconds before executing command...")
-        time.sleep(10)  # Add a 10-second delay
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-        # Execute the command without shell features
-        result = subprocess.run(full_command, check=True, capture_output=True, text=True)
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-        # Determine the output filename
-        output_filename = custom_output_name or os.path.basename(file_path)
-        output_filename += "_" + time.strftime("%Y%m%d_%H%M%S") + ".yml"
+        with open('/app/data/tpdb-set.html', 'w') as file:
+            file.write(response.text)
 
-        # Write the result to the output file
-        output_file_path = os.path.join("/app/data", output_filename)
-        with open(output_file_path, 'w') as output_file:
-            output_file.write(result.stdout)
+        subprocess.run(["python3", "/app/script.py"])
 
-        print(f"Command executed successfully. Output written to: {output_file_path}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing command: {' '.join(full_command)}")
-        print(f"Error details: {e}")
+    except requests.RequestException as e:
+        print(f"Error fetching URL: {e}")
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5432)
+    app.run(host='0.0.0.0', port=5432)
